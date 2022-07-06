4b31df55554ff69899fcb5297fbdaa8bf8d6e667
ryuma017 <tgcrym01@gmail.com>
2022-07-06 23:42:05 +0900

    build telemetry (#3)

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use app::configuration::get_configuration;
 use app::startup::Application;
+use app::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    let subscriber = get_subscriber("hoge".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to read configuration.");
     let application = Application::build(configuration).await?;
     application.run_until_stopped().await?;
